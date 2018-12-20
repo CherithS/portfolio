85bba82d75e9cc76800c8e1b2021d0f66700e26f
cheriths <cherithsimmons@gmail.com>
2018-12-20 12:12:00 -0600

    profile touch-up

--- src/App.js
+++ src/App.js
@@ -17,24 +17,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="card" style={{width: "20vw", height: "85vh", marginTop: '8vh', marginLeft: '5%',position: 'relative', background: 'transparent', marginRight: '3%'}}>
-          <div className="card" style={{height:'40vh', width:'20vw', flexDirection:'column', background: 'transparent' }}>
-            <div className='card' style={{ height:'30vh', width:'inherit', margin: 5 }}>
-              <img style={{height: '27vh'}} src={'https://m.momn.to/0i54Gs_1080x1080.gif'} />
-            </div>
-            <div className='card' style={{ height:'20vh', width:'inherit', margin: 5, display: 'inline', lineHeight: 1.5 }}>
+        <div className="card" style={{width: "20vw", height: "85vh", marginTop: '5vh', marginLeft: '5%',position: 'relative', backgroundColor: 'gold', marginRight: '3%'}}>
+          {/* <div className="card" style={{height:'80vh', width:'20vw', flexDirection:'column', background: 'transparent' }}> */}
+            <div className='card' style={{ height:'85vh', width:'inherit', margin: 5, marginLeft: 15, marginTop: 15, flexDirection:'column', paddingLeft: 10 }}>
+              <h1 style={{fontSize: '2em', margin: 0, marginBottom: 15}} >Profile</h1>
+              <img style={{width:'15vw', border: '5px solid #353434'}} src={'https://m.momn.to/0i54Gs_1080x1080.gif'} />
+              <div style={{ height:'40vh', width:'inherit', margin: 5, display: 'inline', lineHeight: 1.5 }}>
+                <h1 style={{ fontSize: '2em', color: 'orangered', margin: 0 }}>Cherith Simmons</h1>
+                <p style={{ fontSize: '.6em', color: 'mediumblue' }}>FRONT END DEVELOPER</p>
+                <div>
+                  <img src={github} style={{ height: '3vh'}} />
+                  <img src={linkedin} style={{ height: '3vh'}} />
+                  <img src={instagram} style={{ height: '3vh'}} />
+                </div>
+              </div>  
+            {/* </div> */}
+            {/* <div className='card' style={{ height:'20vh', width:'inherit', margin: 5, display: 'inline', lineHeight: 1.5 }}>
               <h1 style={{ fontSize: '2em', color: 'orangered', margin: 0 }}>Cherith Simmons</h1>
               <p style={{ fontSize: '.6em', color: 'mediumblue' }}>FRONT END DEVELOPER</p>
-              <img src={github} style={{ height: '3vh'}} />
-              <img src={linkedin} style={{ height: '3vh'}} />
-              <img src={instagram} style={{ height: '3vh'}} />
-            </div>
-            <div className='card' style={{ height:'35vh', width:'inherit',marginTop: '40vh', position:'absolute' }}></div>
+              <div>
+                <img src={github} style={{ height: '3vh'}} />
+                <img src={linkedin} style={{ height: '3vh'}} />
+                <img src={instagram} style={{ height: '3vh'}} />
+              </div>
+            </div> */}
+            {/* <div className='card' style={{ height:'35vh', width:'inherit',marginTop: '40vh', position:'absolute' }}></div> */}
           </div>          
         </div>
 
 
-        <div className="card" style={{width:"65vw", height: "85vh", marginTop: '8vh', position: 'relative'}} >
+        <div className="card" style={{width:"65vw", height: "85vh", marginTop: '5vh', position: 'relative'}} >
           <div className='card' style={{height:'5vh', width:'inherit', position:'absolute'}}>
             <div style={{marginLeft: '1vw', marginTop: '.3vh'}}>
               <div className='bar'/>
