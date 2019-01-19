ab111f21e74044e6ed0a2d77d2b6273331380d13
cheriths <cherithsimmons@gmail.com>
2019-01-19 10:52:04 -0600

    new design

--- src/App.js
+++ src/App.js
@@ -43,58 +43,45 @@ class App extends Component {
       <div className="App">
 
       {/* PROFILE */}
-        <div className="card" style={{ width: "20vw", height: "85vh", marginTop: '5vh', marginLeft: '5%', position: 'relative', backgroundColor: 'white', marginRight: '3%' }}>
-          <div className='card' style={{ height: '85vh', width: '18vw', margin: 5, marginLeft: 15, marginTop: 15, flexDirection: 'column', padding: '10px 15px' }}>
-            <h1 style={{ fontSize: '2em', margin: 0, marginBottom: 15, marginTop: 15, fontWeight: 600 }} >Profile</h1>
-            <img style={{ width: '15vw', border: '5px solid #353434', marginLeft: '5%' }} src={'https://m.momn.to/0i54Gs_1080x1080.gif'} />
+        <div className="card" style={{ width: "12vw", height: "88vh", marginTop: '5vh', marginLeft: '2vw', position: 'relative', border:'none', backgroundColor: '#FEEFF0', marginRight: '3%' }}>
+          <div className='card' style={{ height: '85vh', width: '10vw', margin: 5, marginLeft: 15, marginTop: 15, flexDirection: 'column', padding: '10px 15px', backgroundColor: 'transparent' }}>
+            <img style={{ width: '10vw', border: '3px solid #ffc9cd', marginLeft: '5%' }} src={'https://m.momn.to/0i54Gs_1080x1080.gif'} />
             <div style={{ height: '40vh', width: 'inherit', margin: 5, display: 'inline', lineHeight: 1.5, textAlign: 'center' }}>
               <div style={{ lineHeight: 2 }}>
-                <h1 style={{ fontSize: '1.5em', color: 'orangered', margin: 0 }}>CHERITH SIMMONS</h1>
-                <div>
-                  <img src={github} style={{ height: '3vh' }} />
-                  <img src={linkedin} style={{ height: '3vh' }} />
-                  <img src={instagram} style={{ height: '3vh' }} />
-                </div>
+                <h1 style={{ fontSize: '1.5em', color: 'mediumblue', margin: 0 }}>CHERITH SIMMONS</h1>
               </div>
               <p style={{ fontSize: '.6em', color: 'mediumblue' }}>I am a FRONT END DEVELOPER.</p> <br />
-              <p style={{ fontSize: '1em', color: 'mediumblue' }}>
+              <p style={{ fontSize: '.7em', color: 'mediumblue', textAlign: 'left' }}>
                 Don't worry.  I'm not here to tell you about how much I love coffee or my favorite breweries.
               </p>
             </div>
-            <div className='card' style={{ height: '5vh', backgroundColor: 'white', border: '5px solid mediumblue', justifyContent: 'center', flexDirection: 'center', alignItems: 'center', cursor: 'pointer' }}>
-              <h4 style={{ color: 'mediumblue' }} >MESSAGE</h4>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <div className='card' style={{width:'10vw', height: '5vh', backgroundColor: 'transparent', border: '5px solid mediumblue', justifyContent: 'center', flexDirection: 'center', alignItems: 'center', cursor: 'pointer' }}>
+                <h4 style={{ color: 'mediumblue' }} >MESSAGE</h4>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '7vw', marginTop: '1vh'}}>
+                <img src={github} style={{ height: '3vh' }} />
+                <img src={linkedin} style={{ height: '3vh' }} />
+                <img src={instagram} style={{ height: '3vh' }} />
+              </div>
             </div>
           </div>
         </div>
 
         {/* MAIN */}
-        <div className="card" style={{ width: "60vw", height: "85vh", marginTop: '5vh', marginLeft: '1%', backgroundColor: 'white' }}>
-          <div className='card' style={{  width: 'inherit', height: '85vh', margin: 5, marginLeft: 15, marginTop: 15, flexDirection: 'column', position:'absolute' }}>
-           {/* NAVIGATION */}
-            <div style={{ height: '5vh', width: 'inherit', border: '1px solid mediumblue', display:'flex', flexDirection: 'row', backgroundColor:'mediumblue' }}>
-              <div style={{display:'flex', height:'5vh', width: '15vw',  border: '1px solid white', }}>
-                <h1 onClick={this.showProjects.bind(this)} style={{ display: 'flex', margin:0, fontSize: '1.5em', color: 'white', cursor: 'pointer', paddingLeft: '15px' }}>PROJECTS</h1>
-              </div>
-              <div style={{display:'flex', height:'5vh', width: '15vw',  border: '1px solid white', }}>
-                <h1 onClick={this.showExperience.bind(this)} style={{ display: 'flex', margin:0, fontSize: '1.5em', color: 'white', cursor: 'pointer', paddingLeft: '15px' }}>EXPERIENCE</h1>
-              </div>
-              <div style={{display:'flex', height:'5vh', width: '15vw',  border: '1px solid white', }}>
-                <h1 style={{ display: 'flex', margin:0, fontSize: '1.5em', color: 'white', cursor: 'pointer', paddingLeft: '15px' }}>Skills</h1>
-              </div>
+        <div style={{display:'flex'}}>
+          <div className="card " style={{ width: "60vw", height: "85vh", marginTop: '5vh', marginLeft: '1%', backgroundColor: 'rgb(254, 239, 240)', border: 'none' }}>
+            <div className='card' style={{  width: 'inherit', height: '85vh', margin: 5, marginLeft: 15, marginTop: 15, flexDirection: 'column', position:'absolute', backgroundColor: 'transparent' }}>
+              
             </div>
+          </div>      
+        </div>
+        
 
-             <div style={{ height: '5vh', width: 'inherit', border: '1px solid mediumblue', display:'flex', flexDirection: 'row', backgroundColor:'white' }}>
-                <ProjectsDropdown />
-            </div>
-
-
-
-            {/* <div style={{ display: 'flex', flexDirection: 'row' }} >
-              { this.state.showProjectsDropdown && <ProjectsDropdown /> }
-              { this.state.showExperienceDropdown && <ExperienceDropdown /> }
-            </div> */}
-
-            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', width:'inherit' }}>
+         {/* SIDE DETAILS */}
+        <div className="card" style={{ width: "12vw", height: "88vh", marginTop: '5vh', marginLeft: '2vw', position: 'relative', border: 'none', backgroundColor: '#FEEFF0', marginRight: '3%' }}>
+          <div className='card' style={{ height: '85vh', width: '10vw', margin: 5, marginLeft: 15, marginTop: 15, flexDirection: 'column', padding: '10px 15px', backgroundColor: 'transparent' }}>
+          <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', width:'inherit' }}>
               <div className="card skills">
                 <h2>React</h2>
               </div>
@@ -162,7 +149,6 @@ class App extends Component {
                 <h2>JSON</h2>
               </div>
             </div>
-
           </div>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class ProjectsDropdown extends Component {
     render() {
-        return (<div style={{ display:'flex', flexDirection: 'row' }}>           
-            <h1 style={{ display: 'flex', width:'15vw', margin: 0, fontSize: '1.5em', color: '#F73F6E', cursor: 'pointer',  border: '1px solid mediumblue', paddingLeft: '15px'  }}>M.A.S.H.</h1>
-            <h1 style={{ display: 'flex', width:'15vw', margin: 0, fontSize: '1.5em', color: '#F73F6E', cursor: 'pointer', border: '1px solid mediumblue', paddingLeft: '15px'  }}>SLO</h1>
-            <h1 style={{ display: 'flex', width:'15vw', margin: 0, fontSize: '1.5em', color: '#F73F6E', cursor: 'pointer', border: '1px solid mediumblue', paddingLeft: '15px'  }}>Personal website</h1>
+        return (<div style={{ display:'flex', flexDirection: 'row', backgroundColor: 'transparent' }}>           
+            <h1 style={{ display: 'flex', width:'15vw', margin: 0, fontSize: '1em', color: '#F73F6E', cursor: 'pointer',  border: '1px solid mediumblue', paddingLeft: '15px', backgroundColor: 'transparent'  }}>M.A.S.H.</h1>
+            <h1 style={{ display: 'flex', width:'15vw', margin: 0, fontSize: '1em', color: '#F73F6E', cursor: 'pointer', border: '1px solid mediumblue', paddingLeft: '15px', backgroundColor: 'transparent'  }}>SLO</h1>
+            <h1 style={{ display: 'flex', width:'15vw', margin: 0, fontSize: '1em', color: '#F73F6E', cursor: 'pointer', border: '1px solid mediumblue', paddingLeft: '15px', backgroundColor: 'transparent'  }}>Personal website</h1>
         </div>         
         );
     }
@@ -17,4 +17,4 @@ export default ProjectsDropdown;
                 //     <div style={{ width:'fit-content', border: '1px solid white', }}><h2>M.A.S.H.</h2></div>
                 //     <div style={{ width:'fit-content', border: '1px solid white', }}><h2>SLO</h2></div>
                 //     <div style={{ width:'fit-content', border: '1px solid white', }}><h2>The very website you're looking at now...</h2></div>
-                // </div> 
+                // </div> , backgroundColor: 'transparent'
