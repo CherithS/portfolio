942405ef2d48ef9f4b0899dd0d3ed2002dc107d6
cheriths <cherithsimmons@gmail.com>
2019-02-10 15:05:51 -0600

    Updates

---
+++ precache-manifest.101c2132797e9ef74503e8a413abb4f0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eff51876822cda0981d61973c9482fc",
+    "url": "/myapp/static/media/pink6.1eff5187.svg"
+  },
+  {
+    "revision": "02959badedf2a225cba3",
+    "url": "/myapp/static/css/main.b33f6bc2.chunk.css"
+  },
+  {
+    "revision": "f35d1da2ba8cc9d117d5",
+    "url": "/myapp/static/js/1.f35d1da2.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "91334e8333e4503ecc3ffd8771f00c60",
+    "url": "/myapp/static/media/pink3.91334e83.svg"
+  },
+  {
+    "revision": "85bd1eb75a90649bcc101f66611b7f97",
+    "url": "/myapp/static/media/pink4.85bd1eb7.svg"
+  },
+  {
+    "revision": "78ac1e35de4e622895abbff8262743aa",
+    "url": "/myapp/static/media/pink5.78ac1e35.svg"
+  },
+  {
+    "revision": "02959badedf2a225cba3",
+    "url": "/myapp/static/js/main.02959bad.chunk.js"
+  },
+  {
+    "revision": "373240a67112e1be92992573c640630a",
+    "url": "/myapp/static/media/pink7.373240a6.svg"
+  },
+  {
+    "revision": "150781653901831d6c2cefd2c4a8a2c7",
+    "url": "/myapp/static/media/pink8.15078165.svg"
+  },
+  {
+    "revision": "8b587271ed2533231d045427740dbe00",
+    "url": "/myapp/static/media/instagram.8b587271.svg"
+  },
+  {
+    "revision": "e1f16a1cc18748b7fc34110d7646b051",
+    "url": "/myapp/static/media/linkedin.e1f16a1c.svg"
+  },
+  {
+    "revision": "568bc32c1c7d9ac9b39126317bf00f26",
+    "url": "/myapp/static/media/github.568bc32c.svg"
+  },
+  {
+    "revision": "fda69f927c8576db4bc182eb15350247",
+    "url": "/myapp/index.html"
+  }
+];
